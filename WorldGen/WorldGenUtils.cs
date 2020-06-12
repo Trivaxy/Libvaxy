5b8a64bbd4ad73c91d5c699bead6db5bb89ad403
Omar Attieh <itsmeomar2003@yahoo.com>
2020-06-12 07:57:16 +0300

    Some worldgen utils

---
+++ WorldGen/WorldGenUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Terraria;
+
+namespace Libvaxy.WorldGen
+{
+	public static class WorldGenUtils
+	{
+		public static void Replace(int x, int y, int width, int height, ushort type, ushort newType)
+		{
+			for (int i = x; i < x + width; i++)
+			{
+				for (int j = y; j < y + height; j++)
+				{
+					if (i < Main.maxTilesX && j < Main.maxTilesY && Main.tile[i, j].type == type)
+					{
+						Main.tile[i, j].active(true);
+						Main.tile[i, j].type = newType;
+					}
+				}
+			}
+		}
+
+		public static void Fill(int x, int y, int width, int height, ushort type)
+		{
+			for (int i = x; i < x + width; i++)
+			{
+				for (int j = y; j < y + height; j++)
+				{
+					Main.tile[i, j].active(true);
+					Main.tile[i, j].type = type;
+				}
+			}
+		}
+
+		public static int[] GetRandomDisplacements(int displacementCount,  float frequency, int maxLimit, float softLimit, int seed)
+		{
+			FastNoise noise = new FastNoise(seed);
+			noise.SetNoiseType(FastNoise.NoiseType.Perlin);
+			noise.SetFrequency(frequency);
+
+			int[] displacements = new int[displacementCount];
+
+			for (int x = 0; x < displacementCount; x++)
+				displacements[x] = (int)Math.Floor(noise.GetNoise(x, x) * maxLimit * softLimit);
+
+			return displacements;
+		}
+	}
+}
