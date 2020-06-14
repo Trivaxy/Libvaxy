cd1f2ee9090fd6bdcce8a15d2f4662e6d3e86e1d
Omar Attieh <itsmeomar2003@yahoo.com>
2020-06-15 01:54:27 +0300

    Add time helper

---
+++ GameHelpers/Time.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+
+namespace Libvaxy.GameHelpers
+{
+	public static class Time
+	{
+		public const int DayLength = 54000;
+		public const int NightLength = 32400;
+
+		public static void SetToDusk()
+		{
+			Main.dayTime = false;
+			Main.time = 0;
+		}
+
+		public static void SetToDawn()
+		{
+			Main.dayTime = true;
+			Main.time = 0;
+		}
+
+		public static void SetToNoon()
+		{
+			Main.dayTime = true;
+			Main.time = DayLength / 2;
+		}
+
+		public static void SetToMidnight()
+		{
+			Main.dayTime = false;
+			Main.time = NightLength / 2;
+		}
+	}
+}
