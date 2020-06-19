7bcd21986a66c2bd5adc2c27f6b113de92717886
Omar Attieh <itsmeomar2003@yahoo.com>
2020-06-19 17:11:36 +0300

    Add ContentHelpers

---
+++ ContentHelpers/SimpleItem.cs
@@ -0,0 +1,58 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Libvaxy.ContentHelpers
+{
+	public class SimpleItem : ModItem
+	{
+		private readonly string name;
+		private readonly string toolTip;
+		private readonly int maxStack;
+		private readonly int value;
+		private readonly int rarity;
+
+		public SimpleItem(string name, string toolTip, int maxStack, int value, int rarity)
+		{
+			this.name = name;
+			this.toolTip = toolTip;
+			this.maxStack = maxStack;
+			this.value = value;
+			this.rarity = rarity;
+		}
+
+		public SimpleItem(string name, int maxStack, int value, int rarity)
+		{
+			this.name = name;
+			this.toolTip = null;
+			this.maxStack = maxStack;
+			this.value = value;
+			this.rarity = rarity;
+		}
+
+		public SimpleItem(string name, int maxStack, int value)
+		{
+			this.name = name;
+			this.toolTip = null;
+			this.maxStack = maxStack;
+			this.value = value;
+			this.rarity = ItemRarityID.White;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault(name);
+
+			if (toolTip != null)
+				Tooltip.SetDefault(toolTip);
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 16;
+			item.height = 16;
+			item.rare = rarity;
+			item.maxStack = maxStack;
+			item.value = value;
+		}
+	}
+}
---
+++ ContentHelpers/SimpleTileItem.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.ID;
+
+namespace Libvaxy.ContentHelpers
+{
+	public class SimpleTileItem : SimpleItem
+	{
+		private readonly int placeType;
+
+		public SimpleTileItem(string name, string toolTip, int maxStack, int value, int rarity, int placeType) : base(name, toolTip, maxStack, value, rarity)
+			=> this.placeType = placeType;
+
+		public SimpleTileItem(string name, int maxStack, int value, int rarity, int placeType) : base(name, maxStack, value, rarity)
+			=> this.placeType = placeType;
+
+		public SimpleTileItem(string name, int maxStack, int value, int placeType) : base(name, maxStack, value)
+			=> this.placeType = placeType;
+
+		public override void SetDefaults()
+		{
+			base.SetDefaults();
+
+			item.createTile = placeType;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.consumable = true;
+			item.useTime = 15;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+		}
+	}
+}
