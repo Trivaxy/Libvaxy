51cff3794b4a2dd3743d0e641b4fa3b3923f37d2
Trivaxy <itsmeomar2003@yahoo.com>
2021-05-19 11:55:45 +0300

    doc PointExtensions

--- Extensions/PointExtensions.cs
+++ Extensions/PointExtensions.cs
@@ -6,13 +6,29 @@ namespace Libvaxy.Extensions
 {
 	public static class PointExtensions
 	{
+		/// <summary>
+		/// Gets a random point within a radius around an origin.
+		/// </summary>
+		/// <param name="point">The origin.</param>
+		/// <param name="radius">The maximum radius away from the origin.</param>
 		public static Point GetRandomPointWithinRadius(this Point point, float radius)
 			=> (_genRand.NextVector2Unit() * _genRand.NextFloat(radius)).ToPoint();
 
+		/// <summary>
+		/// Gets a random point that lies inside a ring around an origin.
+		/// </summary>
+		/// <param name="point">The origin.</param>
+		/// <param name="minRadius">The inner radius of the ring away from the point.</param>
+		/// <param name="maxRadius">The outer radius of the ring away from the point.</param>
 		public static Point GetRandomPointWithinRing(this Point point, float minRadius, float maxRadius)
 			=> (_genRand.NextVector2Unit() * _genRand.NextFloat(minRadius, maxRadius)).ToPoint();
 
-		public static Point GetRandomPointOnCircleAround(this Point point, float radius)
+		/// <summary>
+		/// Gets a random point that lies on the circumference of a circle, around an origin.
+		/// </summary>
+		/// <param name="point">The origin.</param>
+		/// <param name="radius">The radius of the circle.</param>
+		public static Point GetRandomPointOnCircumference(this Point point, float radius)
 			=> (_genRand.NextVector2Unit() * radius).ToPoint();
 	}
 }
